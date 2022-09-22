c4757d10921a430cc07ca7d17b2649e711d5b4c2
peppertree <dragon_ch_z@163.com>
2022-09-22 11:29:30 +0800

    # test

---
+++ src/main/java/com/study/clouddemo2209/Clouddemo2209Application.java
@@ -0,0 +1,18 @@
+package com.study.clouddemo2209;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clouddemo2209Application {
+
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Clouddemo2209Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/clouddemo2209/Clouddemo2209ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.clouddemo2209;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clouddemo2209ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
